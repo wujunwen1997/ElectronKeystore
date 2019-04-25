f58039c9f9231e91b4a93950da6822c9f1b46102
sbwdlihao <sbwdlihao@gmail.com>
2019-04-25 17:10:38 +0800

    在vue中增加ipc测试

--- src/vue-content/src/App.vue
+++ src/vue-content/src/App.vue
@@ -2,16 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="onclicks">Test</button>
+    <button @click="onQueryclicks">Query</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+const { ipcRenderer } =  window.require('electron');
+
+ipcRenderer.on("add-item-result", function (event, arg) {
+  console.log("add-item-result", arg);
+});
+
+ipcRenderer.on("query-all-item-result", function (event, arg) {
+  console.log("query-all-item-result", arg);
+});
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  methods: {
+    onclicks () {
+      ipcRenderer.send("add-item");
+    },
+    onQueryclicks () {
+      ipcRenderer.send("query-all-item");
+    }
   }
 }
 </script>
