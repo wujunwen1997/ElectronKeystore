2ac07c702b58f02d2da06365caea1ae01e6dc8b8
sbwdlihao <sbwdlihao@gmail.com>
2019-05-10 16:55:34 +0800

    key增加p2sh_p2wpkh

--- src/migrate/init.sql
+++ src/migrate/init.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS password (
 
 CREATE TABLE IF NOT EXISTS key (
    pubkey_hash varchar(255) PRIMARY KEY ,
+   p2sh_p2wpkh varchar(255),
    encrypt_key varchar(255) NOT NULL,
    created_at text NOT NULL
 );
+CREATE INDEX redeem_hash ON key (p2sh_p2wpkh);
